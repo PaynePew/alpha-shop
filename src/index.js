31abc71365825a505871281fee80f48bd2cf8143
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-10-21 00:05:08 +0800

    feat: add toggleTheme feature

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function toggleTheme(){
+  // Default to light theme, since that is starting theme
+  window.theme = typeof(window.theme)==='string' ? window.theme : 'theme-light';
+  var switchToTheme = window.theme === 'theme-light' ? 'theme-dark' : 'theme-light';
+  window.theme = switchToTheme;
+  document.querySelector('body').setAttribute('class',switchToTheme);
+}
