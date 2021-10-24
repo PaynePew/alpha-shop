d799544a7440362da23ab0c4509c0c72524749fc
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-10-24 17:56:27 +0800

    feat: add shipping fee

--- src/components/renderCart.mjs
+++ src/components/renderCart.mjs
@@ -1,21 +1,28 @@
 import { cartStore } from "../store/cartStore.mjs";
 import { numbersWithCommas } from "./helpers.mjs";
 
+const cart = document.querySelector(".cart");
+const radio = document.getElementById("customRadio-DHL");
+
 export const renderCart = (cartStore) => {
-  const totalPrice = cartStore.reduce((a, b) => {
+  let totalPrice = cartStore.reduce((a, b) => {
     return a.amount * a.price + b.amount * b.price;
   });
+  let shippingFee = radio.checked ? "$500" : "免運";
+  radio.checked ? (totalPrice += 500) : 0;
+
   const cartFooter = `
     <div class="cart__footer">
       <span class="cart__footerName">運費</span>
-      <span class="cart__price">免運</span>
+      <span class="cart__price">${shippingFee}</span>
     </div>
     <div class="cart__footer">
       <span class="cart__footerName">小計</span>
       <span class="cart__price">$${numbersWithCommas(totalPrice)}</span>
     </div>`;
 
-  return (`<p class="cart__title">購物籃</p>`+
+  return (cart.innerHTML =
+    `<p class="cart__title">購物籃</p>` +
     cartStore
       .map((el) => {
         return `
@@ -36,18 +43,16 @@ export const renderCart = (cartStore) => {
     </div>
   </div>`;
       })
-      .join("") + cartFooter
-  );
+      .join("") +
+    cartFooter);
 };
 
 export const addAmount = (id) => {
   cartStore.map((el) => (el.id === id ? el.amount++ : el));
-  console.log(cartStore);
 };
 
 export const downAmount = (id) => {
   cartStore.map((el) =>
     el.id === id ? (el.amount > 0 ? el.amount-- : el) : el
   );
-  console.log(cartStore);
 };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import toggleTheme from "./components/toggleTheme.mjs";
 import { nextPage, prePage } from "./components/displaySwitch.mjs";
-import { renderCart ,addAmount, downAmount } from "./components/renderCart.mjs";
+import { renderCart, addAmount, downAmount } from "./components/renderCart.mjs";
 import { cartStore } from "./store/cartStore.mjs";
 
 const nextBtn = document.querySelectorAll(".btn--next");
 const preBtn = document.querySelectorAll(".btn--pre");
 const themeButton = document.getElementById("themeButton");
-const cart = document.querySelector(".cart")
+const cart = document.querySelector(".cart");
+const radioButton = document.querySelectorAll(".radioForm__input");
 
 nextBtn.forEach((el) => el.addEventListener("click", nextPage));
 preBtn.forEach((el) => el.addEventListener("click", prePage));
 themeButton.addEventListener("click", toggleTheme);
 
-cart.addEventListener('click', (e) => {
+cart.addEventListener("click", (e) => {
   e.preventDefault();
-  if(e.target.classList.contains('cart__up')){
+  if (e.target.classList.contains("cart__up")) {
     addAmount(Number(e.target.parentElement.id));
   }
-  if(e.target.classList.contains('cart__down')){
+  if (e.target.classList.contains("cart__down")) {
     downAmount(Number(e.target.parentElement.id));
   }
-  cart.innerHTML = renderCart(cartStore);
-})
+  renderCart(cartStore);
+});
 
-cart.innerHTML = renderCart(cartStore);
+radioButton.forEach((el) =>
+  el.addEventListener("click", () => {
+    renderCart(cartStore);
+  })
+);
+
+renderCart(cartStore);
