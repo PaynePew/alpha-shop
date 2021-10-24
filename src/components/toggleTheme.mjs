98de3fc4ffffeda9384d00d46087f117095c39b7
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-10-24 16:02:21 +0800

    style: fix logo & tool theme pics

--- src/components/toggleTheme.mjs
+++ src/components/toggleTheme.mjs
@@ -1,11 +1,10 @@
+const body = document.querySelector("body");
+
 function toggleTheme() {
-  // Default to light theme, since that is starting theme
-  window.theme =
-    typeof window.theme === "string" ? window.theme : "theme-light";
-  const switchToTheme =
-    window.theme === "theme-light" ? "theme-dark" : "theme-light";
-  window.theme = switchToTheme;
-  document.querySelector("body").setAttribute("class", switchToTheme);
+  const switchToTheme = body.classList.contains("theme-light")
+    ? "theme-dark"
+    : "theme-light";
+  body.setAttribute("class", switchToTheme);
 }
 
 export default toggleTheme;
