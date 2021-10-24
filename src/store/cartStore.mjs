f51a2830303c8e642f1c881bee6dcbb882378a08
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-10-24 21:49:02 +0800

    fix: I have faith

--- src/store/cartStore.mjs
+++ src/store/cartStore.mjs
@@ -3,14 +3,14 @@ export const cartStore = [
     id: 1,
     product: '破壞補丁修身牛仔褲',
     price: 3999,
-    picture: '/src/img/item-1@2x.png',
+    picture: 'src/img/item-1@2x.png',
     amount: 1,
   },
   {
     id: 2,
     product: '刷色直筒牛仔褲',
     price: 1299,
-    picture: '/src/img/item-2@2x.png',
+    picture: 'src/img/item-2@2x.png',
     amount: 1,
   }
 ]
